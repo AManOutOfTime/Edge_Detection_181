460ad49835c8c288888f1ee394c5df6957236a2f
Dayton Harvey <74026313+Daytona-23@users.noreply.github.com>
2024-04-16 16:26:19 -0700

    Update RGB_Process.v

--- DE1_SOC_D8M_LB_RTL/V/RGB_Process.v
+++ DE1_SOC_D8M_LB_RTL/V/RGB_Process.v
@@ -17,18 +17,7 @@ module RGB_Process(
 	output reg [7:0] o_VGA_B
 );
 
-  reg [7:0] midpoint =           8'b10000000;
-  reg [7:0] redGray =            8'b00000000;
-  reg [7:0] greenGray =          8'b00000000;
-  reg [7:0] blueGray =           8'b00000000;
-  reg [7:0] intensityTotal =     8'b00000000;
-  reg [7:0] intensityTotalNew =  8'b00000000;
-  reg [7:0] intensityDiff =      8'b00000000;
-  reg [7:0] redAdd =             8'b00000000;
-  reg [7:0] greenAdd =           8'b00000000;
-  reg [7:0] blueAdd =            8'b00000000;
-  int dFactor;
-  int iFactor;
+  reg [7:0] midpoint = 10000000;
 //reg [15:0] square_width = 8;
 //reg [15:0] square_height = 8;
 //reg [15:0] center_row;
@@ -46,16 +35,6 @@ always @(*)begin
 //   square_bottom = center_row + (square_height >> 1);
 //   square_left = center_col - (square_width >> 1);
 //   square_right = center_col + (square_width >> 1);
-dFactor = (Icontrol2 == 0 && Icontrol1 == 0)? 14/16:
-          (Icontrol2 == 0 && Icontrol1 == 1)? 12/16:
-			 (Icontrol2 == 1 && Icontrol1 == 0)? 10/16:
-			 (Icontrol2 == 1 && Icontrol1 == 1)? 8/16:1;
-			 
-iFactor = (Icontrol2 == 0 && Icontrol1 == 0)? 16/16:
-          (Icontrol2 == 0 && Icontrol1 == 1)? 18/16:
-			 (Icontrol2 == 1 && Icontrol1 == 0)? 20/16:
-			 (Icontrol2 == 1 && Icontrol1 == 1)? 22/16:1;			 
-			 
 
 if (row >= 13'd0 && row < 13'd5 && col>=13'd0 && col < 13'd5) begin ///up left - red 
 	o_VGA_R = 8'b11111111;
@@ -84,24 +63,34 @@ end
 //      o_VGA_B  = 8'h00;
 //end
 
-else if (row < 13'd478 && col < 13'd617) begin
-	//convert RGB to grayscale
-	redGray = raw_VGA_R*(.2126);
-	greenGray = raw_VGA_G*(.7152);
-	blueGray = raw_VGA_B*(.0722);
-	intensityTotal = redGray + greenGray + blueGray;
-	//check intensityTotal against midpoint
-	intensityTotalNew = (intensityTotal < midpoint)? (intensityTotal*dFactor):(intensityTotal*iFactor);
-	//find difference between old intensity and new intensity
-	intensityDiff = (intensityTotalNew < intensityTotal) intensityTotal - intensityTotalNew: intensityTotalNew - intensityTotal;
-	//calculate proportion of change of each channel
-	redAdd = intensityDiff/4.7037;
-	greenAdd = intensityDiff/1.3982;
-	blueAdd = intensityDiff/13.85;
-	raw_VGA_R = (brightLevel == 1)? raw_VGA_R + redAdd:raw_VGA_R;
-	raw_VGA_G = (brightLevel == 1)? raw_VGA_G + greenAdd:raw_VGA_G;
-	raw_VGA_B = (brightLevel == 1)? raw_VGA_B + blueAdd:raw_VGA_B;
-	
+else if (row < 13'd478 && col < 13'd617) begin //bottom right - white
+	o_VGA_R = (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 0)? (raw_VGA_R < midpoint)? ((raw_VGA_R*6)/16):(((raw_VGA_R*18)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*18)/16):
+				 (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 1)? (raw_VGA_R < midpoint)? ((raw_VGA_R*4)/16):(((raw_VGA_R*20)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*20)/16):
+				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 0)? (raw_VGA_R < midpoint)? ((raw_VGA_R*3)/16):(((raw_VGA_R*21)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*21)/16):
+				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 1)? (raw_VGA_R < midpoint)? ((raw_VGA_R*1)/16):(((raw_VGA_R*23)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*23)/16):
+				 (rIenable == 0)? raw_VGA_R:
+				 (rIenable == 1 && Icontrol2 == 0 && Icontrol1 == 0)? 8'b00000000:
+	          (rIenable == 1 && Icontrol2 == 0 && Icontrol1 == 1)? raw_VGA_R >> 2:
+				 (rIenable == 1 && Icontrol2 == 1 && Icontrol1 == 0)? raw_VGA_R >> 1:
+				 (rIenable == 1 && Icontrol2 == 1 && Icontrol1 == 1)? raw_VGA_R:raw_VGA_R;
+	o_VGA_G = (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 0)? (raw_VGA_G < midpoint)? ((raw_VGA_G*6)/16):(((raw_VGA_G*18)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*18)/16):
+				 (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 1)? (raw_VGA_G < midpoint)? ((raw_VGA_G*4)/16):(((raw_VGA_G*20)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*20)/16):
+				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 0)? (raw_VGA_G < midpoint)? ((raw_VGA_G*3)/16):(((raw_VGA_G*21)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*21)/16):
+				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 1)? (raw_VGA_G < midpoint)? ((raw_VGA_G*1)/16):(((raw_VGA_G*23)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*23)/16):
+				 (gIenable == 0)? raw_VGA_G:
+				 (gIenable == 1 && Icontrol2 == 0 && Icontrol1 == 0)? 8'b00000000:
+	          (gIenable == 1 && Icontrol2 == 0 && Icontrol1 == 1)? raw_VGA_G >> 2:
+				 (gIenable == 1 && Icontrol2 == 1 && Icontrol1 == 0)? raw_VGA_G >> 1:
+				 (gIenable == 1 && Icontrol2 == 1 && Icontrol1 == 1)? raw_VGA_G:raw_VGA_G;
+	o_VGA_B = (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 0)? (raw_VGA_B < midpoint)? ((raw_VGA_B*6)/16):(((raw_VGA_B*18)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*18)/16):
+				 (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 1)? (raw_VGA_B < midpoint)? ((raw_VGA_B*4)/16):(((raw_VGA_B*20)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*20)/16):
+				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 0)? (raw_VGA_B < midpoint)? ((raw_VGA_B*3)/16):(((raw_VGA_B*21)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*21)/16):
+				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 1)? (raw_VGA_B < midpoint)? ((raw_VGA_B*1)/16):(((raw_VGA_B*23)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*23)/16):
+				 (bIenable == 0)? raw_VGA_B:
+				 (bIenable == 1 && Icontrol2 == 0 && Icontrol1 == 0)? 8'b00000000:
+	          (bIenable == 1 && Icontrol2 == 0 && Icontrol1 == 1)? raw_VGA_B >> 2:
+				 (bIenable == 1 && Icontrol2 == 1 && Icontrol1 == 0)? raw_VGA_B >> 1:
+				 (bIenable == 1 && Icontrol2 == 1 && Icontrol1 == 1)? raw_VGA_B:raw_VGA_B;
 end
 
 else begin //camera out of the range should always be 0
