05c766ff5276a0e9cd6ddd3bee138af773b15a1d
Amaan Mohammed <amaanm.work@gmail.com>
2024-05-07 17:15:25 -0700

    Integrated Cursor and Greyscale/Contrast

--- DE1_SOC_D8M_LB_RTL/DE1_SOC_D8M_LB_RTL.v
+++ DE1_SOC_D8M_LB_RTL/DE1_SOC_D8M_LB_RTL.v
@@ -7,6 +7,8 @@
 //=============================================================================
 
 module DE1_SOC_D8M_LB_RTL (
+	// remove dumb color pixel testers in conrers
+	// remove on switch lighting code
 
    //--- 50 MHz clock from DE1-SoC board
    input          CLOCK_50,
@@ -53,20 +55,13 @@ module DE1_SOC_D8M_LB_RTL (
    output         MIPI_REFCLK,          // 20 MHz from video_pll.v
    output         MIPI_RESET_n
 );
-//parameters for cursor control
-parameter H_LIMIT = 478;
-parameter V_LIMIT = 617;
-parameter VELOCITY = 2;
-parameter R = 4;
-parameter LENGTH = 5;
+
 //=============================================================================
 // reg and wire declarations
 //=============================================================================
-   wire           orequest;
-   wire    [7:0]  raw_VGA_R;
-   wire    [7:0]  raw_VGA_G;
-   wire    [7:0]  raw_VGA_B;
-
+   
+	
+	wire           orequest;
    wire           VGA_CLK_25M;
    wire           RESET_N; 
 	wire           Icontrol1;
@@ -75,25 +70,26 @@ parameter LENGTH = 5;
 	wire           gIenable;
 	wire           bIenable;
 	wire           brightLevel;
-	reg 	[21:0] count = 0;
-	reg 	       c_clk;
-	wire	[7:0]  final_VGA_R;
-	wire	[7:0]  final_VGA_G;
-	wire	[7:0]  final_VGA_B;
-	reg 	[7:0]  input_R;
-	reg 	[7:0]  input_G;
-	reg 	[7:0]  input_B;
-	reg 	[7:0]  next_input_R;
-	reg 	[7:0]  next_input_G;
-	reg 	[7:0]  next_input_B;
-	// cursor bounds
-	reg   signed	[12:0] c_top = 13'd0318;
-	reg   signed   [12:0] c_bottom = 13'd0322;
-	reg   signed   [12:0] c_right = 13'd0242;
-	reg 	signed   [12:0] c_left = 13'd0238;
-	// plus center
-	wire 	 [12:0] c_row;
-	wire 	 [12:0] c_col;
+	wire    [7:0]  raw_VGA_R;
+   wire    [7:0]  raw_VGA_G;
+   wire    [7:0]  raw_VGA_B;
+	reg    [7:0]  reg_raw_VGA_R;
+   reg    [7:0]  reg_raw_VGA_G;
+   reg    [7:0]  reg_raw_VGA_B;
+	wire 	[7:0]  fil_proc_R;
+	wire 	[7:0]  fil_proc_G;
+	wire 	[7:0]  fil_proc_B;
+	reg 	[7:0]  reg_fil_proc_R;
+	reg 	[7:0]  reg_fil_proc_G;
+	reg 	[7:0]  reg_fil_proc_B;
+	wire 	[7:0]  curs_ol__input_R;
+	wire 	[7:0]  curs_ol__input_G;
+	wire 	[7:0]  curs_ol__input_B;
+	reg 	[7:0]  reg_curs_ol__input_R;
+	reg 	[7:0]  reg_curs_ol__input_G;
+	reg 	[7:0]  reg_curs_ol__input_B;
+	
+	
    wire    [7:0]  sCCD_R;
    wire    [7:0]  sCCD_G;
    wire    [7:0]  sCCD_B; 
@@ -117,32 +113,30 @@ assign  LUT_MIPI_PIXEL_HS = MIPI_PIXEL_HS;
 assign  LUT_MIPI_PIXEL_VS = MIPI_PIXEL_VS;
 assign  LUT_MIPI_PIXEL_D  = MIPI_PIXEL_D ;
 
-// calculate plus cursor center
-assign c_row = (c_left + c_right) >> 1; 
-assign c_col = (c_top + c_bottom) >> 1; 
 
-// final colors to input to the monitor (unless its part of the cursor)
-assign	final_VGA_R = raw_VGA_R;
-assign	final_VGA_G = raw_VGA_G;
-assign	final_VGA_B = raw_VGA_B;
-
-assign RESET_N= ~SW[0]; 
+assign RESET_N= 1; 
 assign Icontrol1 = SW[1];
 assign Icontrol2 = SW[2];
-assign bIenable = SW[3];
-assign gIenable = SW[4];
-assign rIenable = SW[5];
-assign brightLevel = SW[6];
+assign Icontrol3 = SW[3];
+assign Icontrol4 = SW[4];
+
+// mode assignment {SW[9], SW[8], SW[7]}
+// 000 normal operation
+// 001 brightLevel
+// 010
+// 011
+// 100
+// 101
+// 110
+// 111
+
+// greyscale/contrast enable
+assign brightLevel = {~SW[9], ~SW[8], SW[7]};
 
 assign MIPI_RESET_n   = RESET_N;
 assign CAMERA_PWDN_n  = RESET_N; 
 assign MIPI_CS_n      = 1'b0; 
 
-//--- Turn on LED[0] when SW[9] up
-assign LEDR[0] = SW[9];
-
-//--- Turn on HEX0[0] when KEY[3] pressed
-assign  HEX0[0] = KEY[3];
 
 //------ MIPI BRIDGE  I2C SETTING--------------- 
 MIPI_BRIDGE_CAMERA_Config cfin(
@@ -185,19 +179,33 @@ D8M_SET   ccd (
 RGB_Process p1 (
 	.Icontrol1(Icontrol1),
 	.Icontrol2(Icontrol2),
-	.rIenable(rIenable),
-	.gIenable(gIenable),
-	.bIenable(bIenable),
+	.Icontrol3(Icontrol3),
+	.Icontrol4(Icontrol4),
 	.brightLevel(brightLevel),
-  	.raw_VGA_R(input_R),
-	.raw_VGA_G(input_G),
-	.raw_VGA_B(input_B),
+  	.raw_VGA_R(reg_raw_VGA_R),
+	.raw_VGA_G(reg_raw_VGA_G),
+	.raw_VGA_B(reg_raw_VGA_B),
    .row      (row),
    .col      (col),
-   .o_VGA_R  (VGA_R),
-   .o_VGA_G  (VGA_G),
-   .o_VGA_B  (VGA_B)
+   .o_VGA_R  (fil_proc_R),
+   .o_VGA_G  (fil_proc_G),
+   .o_VGA_B  (fil_proc_B)
 );
+
+//--- Process monitor cursor
+cursor c_proc(
+	.raw_VGA_R(reg_fil_proc_R), 
+	.raw_VGA_G(reg_fil_proc_G),  
+	.raw_VGA_B(reg_fil_proc_B),  
+	.SW(SW), 
+	.KEY(KEY), 
+	.col(col), 
+	.row(row),
+	.CLOCK_50(CLOCK_50), 
+	.curs_ol__input_R(curs_ol__input_R), 
+	.curs_ol__input_G(curs_ol__input_G), 
+	.curs_ol__input_B(curs_ol__input_B)
+					);
 
 //--- VGA interface signals ---
 assign VGA_CLK    = MIPI_PIXEL_CLK;           // GPIO clk
@@ -209,42 +217,32 @@ assign orequest = ((x_count > 13'd0160 && x_count < 13'd0800 ) &&
 
 // this blanking signal is active low
 assign VGA_BLANK_N = ~((x_count < 13'd0160 ) || ( y_count < 13'd0045 ));
-	
-// cursor color control
-always @ (*)
-begin
-	if(SW[9]) 
-	begin
-		next_input_R = final_VGA_R;
-		next_input_G = final_VGA_G;
-		next_input_B = final_VGA_B;
-	end
-	else
-	begin
-		// plus for cursor
-		// horizontal
-		if((col == c_col) && (row >= c_row - LENGTH/2) && (row <= c_row + LENGTH/2))
-		begin
-			next_input_R = 8'd255;
-			next_input_G = 8'd0;
-			next_input_B = 8'd255;
-		end
-		//vertical
-		else if((row == c_row) && (col >= c_col - LENGTH/2) && (col <= c_col + LENGTH/2))
-		begin
-			next_input_R = 8'd255;
-			next_input_G = 8'd0;
-			next_input_B = 8'd255;
-		end
-		else
-		begin
-			next_input_R = final_VGA_R;
-			next_input_G = final_VGA_G;
-			next_input_B = final_VGA_B;
-		end
-	end		
+
+// connect cursor out direct to VGA
+assign VGA_R = reg_curs_ol__input_R;
+assign VGA_G = reg_curs_ol__input_G;
+assign VGA_B = reg_curs_ol__input_B;
+
+// register module wires
+always @ (posedge CLOCK_50) begin
+	reg_raw_VGA_R <= raw_VGA_R;
+	reg_raw_VGA_G <= raw_VGA_G;
+	reg_raw_VGA_B <= raw_VGA_B;
 end
 
+
+always @ (posedge CLOCK_50) begin
+	reg_fil_proc_R <= fil_proc_R;
+	reg_fil_proc_G <= fil_proc_G;
+	reg_fil_proc_B <= fil_proc_B;
+end
+
+always @ (posedge CLOCK_50) begin
+	reg_curs_ol__input_R <= curs_ol__input_R;
+	reg_curs_ol__input_G <= curs_ol__input_G;
+	reg_curs_ol__input_B <= curs_ol__input_B;
+end
+	
 // generate the horizontal and vertical sync signals
 always @(*) begin
    if ((x_count >= 13'd0002 ) && ( x_count <= 13'd0097))
@@ -257,85 +255,7 @@ always @(*) begin
    else
       VGA_VS = 1'b1;
 end
-// slower clk control to allow for easier cursor movement
-always @ (posedge CLOCK_50)
-begin
-	if(count[20] == 1'b1) 
-	begin  
-		c_clk <= 1'b1;
-		count <= count + 1;
-	end
-	else 
-	begin
-		c_clk <= 1'b0;
-		count <= count + 1;
-	end
-end
-// max vert = 480 max horz = 640
-// changing cursor point (KEY[3] = left, KEY[2] = up, KEY[1] = down, KEY[0] = right)
-always @ (posedge c_clk) 
-begin
-	if(~KEY[3]) 
-	begin
-		if(c_right <= 0 && c_left <= 0)
-		begin
-			c_right <= c_right + H_LIMIT;
-			c_left <= c_left + H_LIMIT;
-		end
-		else
-		begin
-			c_right <= c_right - VELOCITY;
-			c_left <= c_left - VELOCITY;
-		end
-	end
-	if(~KEY[2])
-	begin 	
-		if(c_top <= 0 && c_bottom <= 0)
-		begin
-			c_top <= c_top + V_LIMIT;
-			c_bottom <= c_bottom + V_LIMIT;
-		end
-		else
-		begin
-			c_top <= c_top - VELOCITY;
-			c_bottom <= c_bottom - VELOCITY;
-		end
-	end
-	if(~KEY[1])
-	begin
-		if(c_bottom >= V_LIMIT && c_top >= V_LIMIT)
-		begin
-			c_top <= c_top - V_LIMIT;
-			c_bottom <= c_bottom - V_LIMIT;
-		end
-		else
-		begin
-			c_top <= c_top + VELOCITY;
-			c_bottom <= c_bottom + VELOCITY;
-		end
-	end
-	if(~KEY[0])
-	begin
-		if(c_right >= H_LIMIT && c_left >= H_LIMIT)
-		begin
-			c_right <= c_right - H_LIMIT;
-			c_left <= c_left - H_LIMIT;
-		end
-		else
-		begin
-			c_right <= c_right + VELOCITY;
-			c_left <= c_left + VELOCITY;
-		end
-	end
-end
 
-// color state control
-always @ (posedge CLOCK_50)
-begin
-	input_R <= #1 next_input_R;
-	input_G <= #1 next_input_G;
-	input_B <= #1 next_input_B;
-end
 // calculate col and row as an offset from the x and y counter values
 assign col = x_count - 13'd0164;
 assign row = y_count - 13'd0047;
--- DE1_SOC_D8M_LB_RTL/V/RGB_Process.v
+++ DE1_SOC_D8M_LB_RTL/V/RGB_Process.v
@@ -1,11 +1,9 @@
 module RGB_Process(
 	input  Icontrol1,
 	input  Icontrol2,
-	input  rIenable,
-	input  gIenable,
-	input  bIenable,
+	input  Icontrol3,
+	input  Icontrol4,
 	input  brightLevel,
-	input  clk_slow,
 	input  [7:0] raw_VGA_R,
 	input  [7:0] raw_VGA_G,
 	input  [7:0] raw_VGA_B,
@@ -17,80 +15,83 @@ module RGB_Process(
 	output reg [7:0] o_VGA_B
 );
 
-  reg [7:0] midpoint = 10000000;
-//reg [15:0] square_width = 8;
-//reg [15:0] square_height = 8;
-//reg [15:0] center_row;
-//reg [15:0] center_col;
-//reg [15:0] square_top;
-//reg [15:0] square_bottom;
-//reg [15:0] square_left;
-//reg [15:0] square_right;
+  reg [15:0] midpoint =                16'b0000000010000000;
+  reg [15:0] redGray =                 16'b0000000000000000;
+  reg [15:0] greenGray =               16'b0000000000000000;
+  reg [15:0] blueGray =                16'b0000000000000000;
+  reg [15:0] intensityTotal =          16'b0000000000000000;
+  reg [15:0] intensityTotalNew =       16'b0000000000000000;
+  reg [15:0] intensityDiff =           16'b0000000000000000;
+  reg [15:0] redAdd =                  16'b0000000000000000;
+  reg [15:0] greenAdd =                16'b0000000000000000;
+  reg [15:0] blueAdd =                 16'b0000000000000000;
+  reg [22:0] calcmIntensity =   23'b00000000000000000000000;
+  reg [15:0] meanIntensity =           16'b0000000000000000;
 
-always @(*)begin
+always @(*)begin		 
 
-//	center_row = 240; 
-//   center_col = 320;
-//	square_top = center_row - (square_height >> 1);
-//   square_bottom = center_row + (square_height >> 1);
-//   square_left = center_col - (square_width >> 1);
-//   square_right = center_col + (square_width >> 1);
 
-if (row >= 13'd0 && row < 13'd5 && col>=13'd0 && col < 13'd5) begin ///up left - red 
-	o_VGA_R = 8'b11111111;
-	o_VGA_G = 8'b00000000;
-	o_VGA_B = 8'b00000000;
-end
-
-else if (row >= 13'd0 && row < 13'd5 && col>=13'd613 && col < 13'd617) begin //up right - Green 
-	o_VGA_R = 8'b00000000;
-	o_VGA_G = 8'b11111111;
-	o_VGA_B = 8'b00000000;
-
-end
-
-else if (row >= 13'd474 && row < 13'd478 && col>=13'd0 && col < 13'd5) begin //bottom left - Blue
-	o_VGA_R = 8'b00000000;
-	o_VGA_G = 8'b00000000;
-	o_VGA_B = 8'b11111111;
+if (row < 13'd478 && col < 13'd617) begin
+	//convert RGB to grayscale
+	redGray = ((raw_VGA_R*1063)/5000);
+	greenGray = ((raw_VGA_G*447)/625);
+	blueGray = ((raw_VGA_B*361)/5000);
+	intensityTotal = redGray + greenGray + blueGray;
+	//mean intensity calculation
+	calcmIntensity = calcmIntensity + intensityTotal;
+//	if (row == 13'd477 && col == 13'd616) begin
+//		meanIntensity = calcmIntensity / 19'b1000111101111001000;
+//	end
 	
-end
-
+	meanIntensity = 16'b0000000010000000;
 	
-//else if (row >= square_top && row <= square_bottom && col >= square_left && col <= square_right) begin
-//      o_VGA_R   = 8'h00;
-//      o_VGA_G = 8'hff;
-//      o_VGA_B  = 8'h00;
-//end
-
-else if (row < 13'd478 && col < 13'd617) begin //bottom right - white
-	o_VGA_R = (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 0)? (raw_VGA_R < midpoint)? ((raw_VGA_R*6)/16):(((raw_VGA_R*18)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*18)/16):
-				 (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 1)? (raw_VGA_R < midpoint)? ((raw_VGA_R*4)/16):(((raw_VGA_R*20)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*20)/16):
-				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 0)? (raw_VGA_R < midpoint)? ((raw_VGA_R*3)/16):(((raw_VGA_R*21)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*21)/16):
-				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 1)? (raw_VGA_R < midpoint)? ((raw_VGA_R*1)/16):(((raw_VGA_R*23)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*23)/16):
-				 (rIenable == 0)? raw_VGA_R:
-				 (rIenable == 1 && Icontrol2 == 0 && Icontrol1 == 0)? 8'b00000000:
-	          (rIenable == 1 && Icontrol2 == 0 && Icontrol1 == 1)? raw_VGA_R >> 2:
-				 (rIenable == 1 && Icontrol2 == 1 && Icontrol1 == 0)? raw_VGA_R >> 1:
-				 (rIenable == 1 && Icontrol2 == 1 && Icontrol1 == 1)? raw_VGA_R:raw_VGA_R;
-	o_VGA_G = (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 0)? (raw_VGA_G < midpoint)? ((raw_VGA_G*6)/16):(((raw_VGA_G*18)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*18)/16):
-				 (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 1)? (raw_VGA_G < midpoint)? ((raw_VGA_G*4)/16):(((raw_VGA_G*20)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*20)/16):
-				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 0)? (raw_VGA_G < midpoint)? ((raw_VGA_G*3)/16):(((raw_VGA_G*21)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*21)/16):
-				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 1)? (raw_VGA_G < midpoint)? ((raw_VGA_G*1)/16):(((raw_VGA_G*23)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*23)/16):
-				 (gIenable == 0)? raw_VGA_G:
-				 (gIenable == 1 && Icontrol2 == 0 && Icontrol1 == 0)? 8'b00000000:
-	          (gIenable == 1 && Icontrol2 == 0 && Icontrol1 == 1)? raw_VGA_G >> 2:
-				 (gIenable == 1 && Icontrol2 == 1 && Icontrol1 == 0)? raw_VGA_G >> 1:
-				 (gIenable == 1 && Icontrol2 == 1 && Icontrol1 == 1)? raw_VGA_G:raw_VGA_G;
-	o_VGA_B = (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 0)? (raw_VGA_B < midpoint)? ((raw_VGA_B*6)/16):(((raw_VGA_B*18)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*18)/16):
-				 (brightLevel == 1 && Icontrol2 == 0 && Icontrol1 == 1)? (raw_VGA_B < midpoint)? ((raw_VGA_B*4)/16):(((raw_VGA_B*20)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*20)/16):
-				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 0)? (raw_VGA_B < midpoint)? ((raw_VGA_B*3)/16):(((raw_VGA_B*21)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*21)/16):
-				 (brightLevel == 1 && Icontrol2 == 1 && Icontrol1 == 1)? (raw_VGA_B < midpoint)? ((raw_VGA_B*1)/16):(((raw_VGA_B*23)/16) > 8'b11111111)? 8'b11111111:((raw_VGA_R*23)/16):
-				 (bIenable == 0)? raw_VGA_B:
-				 (bIenable == 1 && Icontrol2 == 0 && Icontrol1 == 0)? 8'b00000000:
-	          (bIenable == 1 && Icontrol2 == 0 && Icontrol1 == 1)? raw_VGA_B >> 2:
-				 (bIenable == 1 && Icontrol2 == 1 && Icontrol1 == 0)? raw_VGA_B >> 1:
-				 (bIenable == 1 && Icontrol2 == 1 && Icontrol1 == 1)? raw_VGA_B:raw_VGA_B;
+	//check intensityTotal against midpoint
+	intensityTotalNew = (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd16)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd16)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd17)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd17)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd18)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd18)/16'd16): 
+							  (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd19)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd19)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd20)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd20)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd21)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd21)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd22)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd22)/16'd16): 
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd23)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd23)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd24)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd24)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd25)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd25)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd26)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd26)/16'd16): 
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd27)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd27)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd28)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd28)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd29)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd29)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 0 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd30)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd30)/16'd16): 
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 1 && (intensityTotal > meanIntensity))?  (((intensityTotal*16'd31)/16'd16) > 8'b11111111)? 8'b11111111:((intensityTotal*16'd31)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 0)?  (((intensityTotal*16'd16)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd16)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 1)?  (((intensityTotal*16'd15)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd15)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 0)?  (((intensityTotal*16'd14)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd14)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 1)?  (((intensityTotal*16'd13)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd13)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 0)?  (((intensityTotal*16'd12)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd12)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 1)?  (((intensityTotal*16'd11)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd11)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 0)?  (((intensityTotal*16'd10)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd10)/16'd16):
+							  (Icontrol4 == 0 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 1)?  (((intensityTotal*16'd9)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd9)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 0)?  (((intensityTotal*16'd8)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd8)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 0 && Icontrol1 == 1)?  (((intensityTotal*16'd7)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd7)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 0)?  (((intensityTotal*16'd6)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd6)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 0 && Icontrol2 == 1 && Icontrol1 == 1)?  (((intensityTotal*16'd5)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd5)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 0)?  (((intensityTotal*16'd4)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd4)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 0 && Icontrol1 == 1)?  (((intensityTotal*16'd3)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd3)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 0)?  (((intensityTotal*16'd2)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd2)/16'd16):
+							  (Icontrol4 == 1 && Icontrol3 == 1 && Icontrol2 == 1 && Icontrol1 == 1)?  (((intensityTotal*16'd1)/16'd16) < 8'b00000000)? 8'b00000000:((intensityTotal*16'd1)/16'd16):((intensityTotal*16'd16)/16'd16);
+							  
+	//find difference between old intensity and new intensity
+	o_VGA_R = (brightLevel == 1)? (intensityTotalNew > 8'b11111111)? 8'b11111111:intensityTotalNew:raw_VGA_R;
+   o_VGA_G = (brightLevel == 1)? (intensityTotalNew > 8'b11111111)? 8'b11111111:intensityTotalNew:raw_VGA_G;
+   o_VGA_B = (brightLevel == 1)? (intensityTotalNew > 8'b11111111)? 8'b11111111:intensityTotalNew:raw_VGA_B;
+//	intensityDiff = (intensityTotalNew < intensityTotal)? intensityTotal - intensityTotalNew: intensityTotalNew - intensityTotal;
+//	//calculate proportion of change of each channel
+//	redAdd = (intensityDiff*1000)/(4000 + 737);
+//	greenAdd = (intensityDiff*5000)/(5000 + 1991);
+//	blueAdd = (intensityDiff*20)/(260 + 17);
+//	o_VGA_R = (brightLevel == 1)? ((raw_VGA_R + redAdd) > 8'b11111111)? 8'b11111111:(raw_VGA_R + redAdd):raw_VGA_R;
+//	o_VGA_G = (brightLevel == 1)? ((raw_VGA_G + greenAdd) > 8'b11111111)? 8'b11111111:(raw_VGA_G + greenAdd):raw_VGA_G;
+//	o_VGA_B = (brightLevel == 1)? ((raw_VGA_B + blueAdd) > 8'b11111111)? 8'b11111111:(raw_VGA_B + blueAdd):raw_VGA_B;
+	
 end
 
 else begin //camera out of the range should always be 0
@@ -100,33 +101,5 @@ else begin //camera out of the range should always be 0
 
 	end
 end
-
-
-////cursor controller
-//always @(posedge clk_slow) begin
-//	if (cursRight) begin
-//		square_right <= square_right + 4;
-//		square_left <= square_left + 4;
-//		
-//    end 
-//	 else if (cursLeft) begin
-//		square_right <= square_right - 4;
-//		square_left <= square_left - 4;
-//	end 
-//	else if (cursDown) begin
-//		square_top <= square_top + 4;
-//      square_bottom <= square_bottom + 4;
-//	end 
-//	else if (cursUp) begin
-//		square_top <= square_top - 4;
-//      square_bottom <= square_bottom - 4;
-//	end
-//	else begin
-//		square_right <= square_right;
-//		square_left <= square_left;
-//		square_top <= square_top;
-//		square_bottom <= square_bottom;
-//	end
-//end
 
 endmodule
